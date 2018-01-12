4e0a1d0474916d27b0778cb8742d6d9acefb2fb1
a298582548 <105021010@live.asia.edu.tw>
2018-01-12 15:30:55 +0800

    圖鑑完成

--- bugsExpelled/src/Illustrated.java
+++ bugsExpelled/src/Illustrated.java
@@ -1,18 +1,34 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 public class Illustrated extends JFrame{
 
     private Container cp;
-    private JLabel lb1 = new JLabel("1");
+
+    private JLabel lb1 = new JLabel("");
     public Illustrated(){
         initComp();
     }
     private void initComp(){
 
-        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        cp=this.getContentPane();
-        this.setBounds(500,500,500,500);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                MainFrame mram = new MainFrame();
+                mram.setVisible(true);
+                Illustrated.this.setVisible(false);
+            }
+        });
 
+
+
+        cp=this.getContentPane();
+        this.setBounds(500,0,1000,1000);
+        cp.add(lb1);
+        ImageIcon illustrated = new ImageIcon("graphic/illustrated.png");
+        lb1.setIcon(illustrated);
     }
 }
